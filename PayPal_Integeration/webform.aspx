763f7ed272b55115adce14878beaea7675128037
Adil Waheed <adilwaheed2222.com@gmail.com>
2023-07-10 16:33:25 +0500

    initial commit

---
+++ PayPal_Integeration/webform.aspx
@@ -0,0 +1,217 @@
+﻿<%@ Page Async="true" Language="C#" AutoEventWireup="true" CodeBehind="webform.aspx.cs" Inherits="PayPal_Integeration.webform" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Donation Page</title>
+    <style>
+        .gdlr-paypal-form-head {
+            font-size: 24px;
+            font-weight: bold;
+            margin-bottom: 20px;
+        }
+
+        .gdlr-paypal-amount-wrapper {
+            margin-bottom: 20px;
+        }
+
+        .gdlr-head {
+            font-size: 18px;
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
+
+        .gdlr-amount-button {
+            display: inline-block;
+            padding: 5px 10px;
+            background-color: #ebebeb;
+            color: #333;
+            text-decoration: none;
+            margin-right: 10px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+            .gdlr-amount-button.active {
+                background-color: #333;
+                color: #fff;
+            }
+
+        .custom-amount {
+            width: 50%;
+            padding: 5px;
+            margin-top: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        .gdlr-recurring-payment-wrapper {
+            margin-top: 20px;
+        }
+
+        .gdlr-subhead {
+            font-size: 16px;
+        }
+
+        .gdlr-recurring-option {
+            margin-left: 10px;
+        }
+
+        .gdlr-recurring-time-wrapper {
+            margin-top: 10px;
+        }
+
+        input[type="submit"] {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+            input[type="submit"]:hover {
+                background-color: #555;
+            }
+    </style>
+    <style>
+        #paypal-button-container {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background-color: #fff;
+            border: 1px solid #ccc;
+            padding: 20px;
+            box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+            z-index: 9999;
+            display: none; /* Hide the container by default */
+        }
+
+        .backdrop {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5);
+            z-index: 9998;
+            display: none; /* Hide the backdrop by default */
+        }
+    </style>
+  <script src="https://www.paypal.com/sdk/js?client-id=YOUR_CLIENT_ID&vault=true&intent=subscription">
+  </script> 
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="gdlr-paypal-amount-wrapper">
+            <span class="gdlr-head">How much would you like to donate?</span>
+            <input type="text" class="custom-amount" placeholder="Enter Amount In $" id="userAmount" runat="server" onkeypress="return isNumberKey(event)">
+            <label id="errorMessage" style="color: red; display: none;"></label>
+            <label id="amountErrorMessage" style="color: red; display: none;">Please enter a  Amount.</label>
+            <div class="clear"></div>
+
+            <!-- recurring-1 -->
+            <div class="gdlr-recurring-payment-wrapper">
+                <span class="gdlr-head">Would you like to make regular donations?</span>
+                <span class="gdlr-subhead">I would like to make </span>
+                <asp:DropDownList ID="ddlRecurringPeriod" runat="server" CssClass="gdlr-recurring-option" onchange="showHideRecurringTimes()">
+                    <asp:ListItem Value="0">a one time</asp:ListItem>
+                    <asp:ListItem Value="W">weekly</asp:ListItem>
+                    <asp:ListItem Value="M">monthly</asp:ListItem>
+                    <asp:ListItem Value="Y">yearly</asp:ListItem>
+                </asp:DropDownList>
+                <span class="gdlr-subhead">donation(s)</span>
+                <div class="gdlr-recurring-time-wrapper" id="recurringTimesWrapper" style="display: none">
+                    <span class="gdlr-subhead">How many times would you like this to recur? (including this payment)*</span>
+                    <asp:DropDownList ID="ddlRecurringTimes" runat="server" CssClass="gdlr-recurring-option">
+                        <asp:ListItem Value="2">2</asp:ListItem>
+                        <asp:ListItem Value="3">3</asp:ListItem>
+                        <asp:ListItem Value="4">4</asp:ListItem>
+                        <asp:ListItem Value="5">5</asp:ListItem>
+                        <asp:ListItem Value="6">6</asp:ListItem>
+                        <asp:ListItem Value="7">7</asp:ListItem>
+                        <asp:ListItem Value="8">8</asp:ListItem>
+                        <asp:ListItem Value="9">9</asp:ListItem>
+                        <asp:ListItem Value="10">10</asp:ListItem>
+                        <asp:ListItem Value="11">11</asp:ListItem>
+                        <asp:ListItem Value="12">12</asp:ListItem>
+                    </asp:DropDownList>
+                </div>
+
+            </div>
+            <!-- recurring-2 -->
+        </div>
+        <div>
+            <asp:Button ID="btnDonate" runat="server" Text="Donate" OnClick="btnDonate_Click1" OnClientClick="return validateAmount()" />
+        </div>
+    </form>
+    <div id="paypal-button-container"></div>
+    <div class="backdrop"></div>
+</body>
+<script type="text/javascript">
+    function isNumberKey(evt) {
+        var charCode = (evt.which) ? evt.which : event.keyCode;
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+            document.getElementById("errorMessage").innerHTML = "Please enter a valid number";
+            document.getElementById("errorMessage").style.display = "block";
+            return false;
+        }
+        document.getElementById("errorMessage").style.display = "none";
+        return true;
+    }
+    function showHideRecurringTimes() {
+        var recurringPeriod = document.getElementById('<%= ddlRecurringPeriod.ClientID %>').value;
+        var recurringTimesWrapper = document.getElementById('recurringTimesWrapper');
+
+        if (recurringPeriod === '0') {
+            recurringTimesWrapper.style.display = 'none';
+        } else {
+            recurringTimesWrapper.style.display = 'block';
+        }
+    }
+    function validateAmount() {
+        var amount = document.getElementById('userAmount').value;
+        var amountErrorMessage = document.getElementById('amountErrorMessage');
+
+        if (amount === null || amount.trim() === '') {
+            amountErrorMessage.style.display = 'block';
+            return false;
+        }
+
+        amountErrorMessage.style.display = 'none';
+        return true;
+    }
+    document.getElementById('userAmount').addEventListener('input', function () {
+        var amountErrorMessage = document.getElementById('amountErrorMessage');
+        amountErrorMessage.style.display = 'none';
+    });
+</script>
+<script>
+    var subscriptionPlanId = '<%= Session["SubscriptionPlanId"] %>';
+    if (subscriptionPlanId === '') {
+        // Hide the button if the plan ID is empty
+        document.querySelector('#paypal-button-container').style.display = 'none';
+        document.querySelector('.backdrop').style.display = 'none'; // Hide the backdrop
+    } else {
+        document.querySelector('#paypal-button-container').style.display = 'block';
+        document.querySelector('.backdrop').style.display = 'block';
+        paypal.Buttons({
+            createSubscription: function (data, actions) {
+                return actions.subscription.create({
+                    'plan_id': subscriptionPlanId // Creates the subscription
+                });
+            },
+            onApprove: function (data, actions) {
+                // Make a server-side call to clear the session value
+                document.querySelector('#paypal-button-container').style.display = 'none';
+                document.querySelector('.backdrop').style.display = 'none'; // Hide the backdrop
+                //alert('You have successfully subscribed to ' + data.subscriptionID); // Optional message given to subscriber
+
+            }
+        }).render('#paypal-button-container'); // Renders the PayPal button
+    }
+</script>
+
+</html>
---
+++ PayPal_Integeration/webform.aspx.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace PayPal_Integeration
+{
+    public partial class webform : System.Web.UI.Page
+    {
+        public  string clientId = ConfigurationManager.AppSettings["PayPalClientId"];
+        public string clientSecret = ConfigurationManager.AppSettings["PayPalClientSecret"];
+        public string emailAddress = ConfigurationManager.AppSettings["EmailAddress"];
+        public static string baseurl = ConfigurationManager.AppSettings["base_url"];
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //bool isSubscriptionSuccessful = true; // Set to true or false based on your logic
+
+            //if (isSubscriptionSuccessful)
+            //{
+            //    // Clear the session value
+            //    Session["SubscriptionPlanId"] = null;
+            //}
+        }
+        private static async Task<string> GetAccessToken(string clientId, string clientSecret)
+        {
+            string tokenEndpoint = $"{baseurl}/v1/oauth2/token";
+
+            // Set up the request parameters
+            var requestData = new Dictionary<string, string>
+              {
+                  { "grant_type", "client_credentials" }
+              };
+            var requestContent = new FormUrlEncodedContent(requestData);
+
+            // Create a Base64-encoded string of the client credentials
+            string credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}"));
+
+            // Set up the HTTP client
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
+
+            // Send the POST request to the token endpoint
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+            HttpResponseMessage response = await client.PostAsync(tokenEndpoint, requestContent);
+
+            // Process the response
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+                dynamic responseData = JsonConvert.DeserializeObject(responseContent);
+
+                // Extract and return the access token
+                string accessToken = responseData.access_token;
+                return accessToken;
+            }
+            else
+            {
+                // Handle the case when the token request fails
+                string errorContent = await response.Content.ReadAsStringAsync();
+                // Log or display an error message
+                return null;
+            }
+        }
+        private static async Task<string> CreateProduct(string accessToken)
+        {
+            string apiUrl = $"{baseurl}/v1/catalogs/products";
+
+            // Set up the request payload
+            var product = new
+            {
+                name="Donate A Money",
+                type= "SERVICE",
+                description="Donation For Needy People",
+                category= "SERVICES"
+            };
+
+            // Convert the product object to JSON
+            string jsonPayload = JsonConvert.SerializeObject(product);
+
+            // Set up the HTTP client
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            // Send the POST request to create the product
+            StringContent content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+            // Process the response
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+                dynamic responseData = JsonConvert.DeserializeObject(responseContent);
+
+                // Extract and return the product ID
+                string productId = responseData.id;
+                return productId;
+            }
+            else
+            {
+                // Handle the case when the product creation fails
+                string errorContent = await response.Content.ReadAsStringAsync();
+                // Log or display an error message
+                return null;
+            }
+        }
+
+        protected async void btnDonate_Click1(object sender, EventArgs e)
+        {
+            decimal amount = decimal.Parse(userAmount.Value);
+            bool isRecurringPayment = ddlRecurringPeriod.SelectedValue != "0";
+            var frequency = ddlRecurringPeriod.SelectedItem.Text;
+            var recurrenceCount = ddlRecurringTimes.SelectedItem.Text;
+            // Store the donation amount in a session variable or pass it as a query parameter to the checkout page
+            Session["DonationAmount"] = amount;
+
+            // Redirect to the PayPal checkout page
+            string paypalUrl = "https://www.paypal.com/cgi-bin/webscr";
+            string businessEmail = emailAddress; // Replace with your PayPal business email
+            string itemName = "Testing Donation";
+            string returnURL = "https://example.com/success"; // Replace with your success return URL
+            string cancelURL = "https://example.com/cancel"; // Replace with your cancel return URL
+
+
+
+            if (isRecurringPayment)
+            {
+                // Create PayPal subscription
+
+                // Set up the API request payload
+                string apiEndpoint = $"{baseurl}/v1/billing/plans";
+                string accessToken = await GetAccessToken(clientId, clientSecret); // Replace with your access token
+                string productID = await CreateProduct(accessToken); // Replace with your product ID
+                string planName = "Subscription Plan";
+                string planDescription = "Subscription plan for recurring donations";
+                string currencyCode = "USD";
+
+                // Create the billing cycle object based on the selected frequency and recurrence count
+                var billingCycles = new List<object>();
+                switch (frequency)
+                {
+                    case "weekly":
+                        billingCycles.Add(new
+                        {
+                            frequency = new
+                            {
+                                interval_unit = "WEEK",
+                                interval_count = 1
+                            },
+                            tenure_type = "REGULAR",
+                            sequence = 1,
+                            total_cycles = int.Parse(recurrenceCount),
+                            pricing_scheme = new
+                            {
+                                fixed_price = new
+                                {
+                                    value = amount.ToString(),
+                                    currency_code = currencyCode
+                                }
+                            }
+                        });
+                        break;
+                    case "monthly":
+                        billingCycles.Add(new
+                        {
+                            frequency = new
+                            {
+                                interval_unit = "MONTH",
+                                interval_count = 1
+                            },
+                            tenure_type = "REGULAR",
+                            sequence = 1,
+                            total_cycles = int.Parse(recurrenceCount),
+                            pricing_scheme = new
+                            {
+                                fixed_price = new
+                                {
+                                    value = amount.ToString(),
+                                    currency_code = currencyCode
+                                }
+                            }
+                        });
+                        break;
+                    case "yearly":
+                        billingCycles.Add(new
+                        {
+                            frequency = new
+                            {
+                                interval_unit = "YEAR",
+                                interval_count = 1
+                            },
+                            tenure_type = "REGULAR",
+                            sequence = 1,
+                            total_cycles = int.Parse(recurrenceCount),
+                            pricing_scheme = new
+                            {
+                                fixed_price = new
+                                {
+                                    value = amount.ToString(),
+                                    currency_code = currencyCode
+                                }
+                            }
+                        });
+                        break;
+                }
+                // Construct the request payload
+                var requestPayload = new
+                {
+                    product_id = productID,
+                    name = planName,
+                    description = planDescription,
+                    billing_cycles = billingCycles,
+                    payment_preferences = new
+                    {
+                        auto_bill_outstanding = true,
+                        setup_fee = new
+                        {
+                            value = "0",
+                            currency_code = currencyCode
+                        },
+                        setup_fee_failure_action = "CONTINUE",
+                        payment_failure_threshold = 3
+                    }
+                };
+                // Convert the request payload to JSON
+                string jsonPayload = JsonConvert.SerializeObject(requestPayload);
+
+                // Make the API call to create the subscription plan
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                StringContent content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+                HttpResponseMessage response = await client.PostAsync(apiEndpoint, content);
+                // Check if the API call was successful
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    dynamic responseData = JsonConvert.DeserializeObject(responseContent);
+
+                    // Retrieve the subscription plan ID from the response data
+                    string planId = responseData.id;
+
+                    // Store the plan ID in a session variable or pass it as a query parameter to the checkout page
+                    Session["SubscriptionPlanId"] = planId;
+
+
+                }
+                else
+                {
+                    // Handle the case when the API call fails
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    // Log or display an error message to the user
+                }
+
+            }
+            else
+            {
+                // Build the query string parameters
+                string redirectUrl = $"{paypalUrl}?cmd=_xclick&business={businessEmail}&item_name={itemName}&amount={amount}&return={returnURL}&cancel_return={cancelURL}";
+                // Redirect the user to the PayPal checkout page
+                Response.Redirect(redirectUrl, false);
+            }
+
+
+        }
+    }
+}
